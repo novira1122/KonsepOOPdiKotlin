af3146b9e744333d962fc3073c68609a5ef84949
Modern 14 <noviraasnani506@gmail.com>
2024-09-23 17:28:03 +0700

    add Greeter.kt

---
+++ src/main/kotlin/id/ac/polbeng/nvoviraasnani/test_kelas/Greeter.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.nvoviraasnani.test_kelas
+
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    }
+}
---
+++ src/main/kotlin/id/ac/polbeng/nvoviraasnani/test_kelas/GreeterTest.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.nvoviraasnani.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+}
